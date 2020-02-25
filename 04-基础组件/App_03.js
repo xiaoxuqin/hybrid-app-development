be3e90a8b45c42ba1b763b97fa412233008ee62b
秦晓旭 <33643037+xiaoxuqin@users.noreply.github.com>
2020-02-25 15:23:02 +0800

    App.js_03(像素)

---
+++ 04-基础组件/App_03.js
@@ -0,0 +1,62 @@
+import React,{useState,useEffect} from 'react';
+import {
+	SafeAreaView,
+	StyleSheet,
+	View,//相当于div
+	Text,
+	Image,
+	TextInput,
+	Button,
+	ScrollView,
+	ImageBackground,
+	StatusBar,
+	Dimensions,
+	PixelRatio,
+	BackHandler,
+	ToastAndroid
+} from 'react-native';
+
+const styles = StyleSheet.create({
+	box:{
+		width:100,
+		height:100,
+		margin: 10,
+		borderColor:'red',
+		borderWidth:1,
+	},
+	box1:{
+		width:100,
+		height:100,
+		margin: 10,
+		borderColor:'red',
+		borderWidth:1/1.5,
+	},
+	txt:{
+		color:"red",
+	},
+	size:{
+		fontSize:30
+	}
+});
+const App = () => {
+	const {width,height,scale} = Dimensions.get('window');
+	console.log(width,height,scale);
+	console.log(PixelRatio.get())
+	// px:图片中最小的一格
+	// dpi（dot per inch）:每英寸有多少小格（1px）
+	// dp:在安卓开发中用的单位，1dp等于像素密度为160dpi时1px的大小。
+	// dpi:     120 160 240 320 480
+	// 密度比    0.75  1  1.5  2   3
+
+	return (
+		<>
+			<StatusBar barStyle="dark-content" />
+			<SafeAreaView>
+				<View style={styles.box}></View>
+				<View style={styles.box1}></View>
+			</SafeAreaView>
+		</>
+	);
+};
+
+export default App;
